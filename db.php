783688b7247ffd34ee7fce8f2ccd3571987508bb
AlexCauich <63112468+AlexCauich@users.noreply.github.com>
2020-05-03 16:02:37 -0500

    first commit

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+//connect to the database 
+$db = new mysqli('localhost', 'root', '', 'travels');
+
+/*if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";*/
+
+?>
